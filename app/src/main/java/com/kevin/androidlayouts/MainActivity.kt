be1fc2f2846444f12d9b6796342b7eb556cb8464
A.v.s.Pavan <132935837+Kris777na@users.noreply.github.com>
2023-10-05 11:19:52 -0500

    Added assignment for linear layout

--- app/src/main/java/com/kevin/androidlayouts/MainActivity.kt
+++ app/src/main/java/com/kevin/androidlayouts/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.kevin.androidlayouts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kevin.androidlayouts.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val mainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(mainBinding.root)
+        initViews()
+    }
+
+    private fun initViews() {
+        mainBinding.linearBtn.setOnClickListener {
+            val intent = Intent(this, LinearLayout::class.java)
+            startActivity(intent)
+        }
+
+        mainBinding.linearAssignmentBtn.setOnClickListener {
+            val intent = Intent(this, LinearAssignment::class.java)
+            startActivity(intent)
+        }
     }
 }
